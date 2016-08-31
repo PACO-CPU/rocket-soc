505c32a60a55a048e8f5cecb93e12ff232523e05
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2016-08-31 11:15:29 +0200

    lut-gauss-app: Cleanup & cycle measuring
    this commit refactors the lut, native, and debug code into
    seperate functions. It also add code to measure cycles. This can
    be invoked by make run-time.
    
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- rocket_soc/lib/templates/lut-gaussian-application/main.c
+++ rocket_soc/lib/templates/lut-gaussian-application/main.c
@@ -8,6 +8,7 @@
 int main_();
 int main() { return main_(); }
 //#define LUT
+//#define DEBUG
 #define IMG_WIDTH 64
 #define IMG_HEIGHT 64
 #define IMG_RESULT_SIZE (IMG_WIDTH) * (IMG_HEIGHT)
@@ -32,67 +33,34 @@ uint64_t kernel[9] = {1, 3, 1,
                       3, 9, 3,
                       1, 3, 1,};
 
+uint64_t get_cycles() {
+  uint64_t r;
+  asm("rdcycle %0" : "=r"(r));
+  return r;
+}
+
 void write_raw_image_header()
 {
     char buf[64];
     int r = 0;
 
     r += wrstring(buf + r, "//(");
-#ifdef LUT
-    r += wruint64(buf + r, IMG_WIDTH-2);
-    r += wrstring(buf + r, ",");
-    r += wruint64(buf + r, IMG_HEIGHT);
-#else
     r += wruint64(buf + r, IMG_WIDTH-2);
     r += wrstring(buf + r, ",");
     r += wruint64(buf + r, IMG_HEIGHT-2);
-#endif
     r += wrstring(buf + r, ")\nuint64_t img[] = {");
-    
+
     uart_println(buf);
 }
 
-int main_()
+void gauss_native()
 {
-
     int result, value1, value2, value3;
-    int x, y, i;
     int intermediate[IMG_RESULT_SIZE];
-
     int r;
     char buf[128];
-    write_raw_image_header(); 
-#ifdef LUT
-    /* Horizontal */
-    for (i = 1; i < IMG_WIDTH * IMG_HEIGHT; i++) {
-        value1= image[i-1];
-        value2= image[i];
-        value3= image[i+1];
-        result = LUTE3(0, value1, value2, value3);
+    int x, y, i;
 
-        intermediate[i-1] = result;
-    }
-
-    for (x = 1; x < IMG_WIDTH; x++) {
-
-        for (y = 1; y < IMG_HEIGHT - 1; y++) {
-            value1 = intermediate[(y-1) * IMG_WIDTH + x];
-            value2 = intermediate[(y  ) * IMG_WIDTH + x];
-            value3 = intermediate[(y+1) * IMG_WIDTH + x];
-            result = LUTE3(0, value1, value2, value3);
-            /* print single result to uart */
-            r=0;
-            r+=wruint64(buf+r,result);
-            r+=wrstring(buf+r,",");
-            uart_println(buf);
-
-        }
-
-    }
-    r = 0;
-    r += wrstring(buf + r, "};");
-    uart_println(buf);
-#else
     for (x = 1; x < IMG_WIDTH - 1; x++) {
         for (y = 1; y < IMG_HEIGHT - 1; y++) {
             result  = image[(y - 1) * IMG_WIDTH + (x - 1)] * kernel[0];
@@ -113,7 +81,93 @@ int main_()
             uart_println(buf);
         }
     }
+
+}
+
+void gauss_lut()
+{
+    int result, value1, value2, value3;
+    int intermediate[IMG_RESULT_SIZE];
+    int r;
+    char buf[128];
+    int x, y, i;
+    /* Horizontal */
+    for (i = 1; i < (IMG_WIDTH * IMG_HEIGHT)-1; i++) {
+        value1= image[i-1];
+        value2= image[i];
+        value3= image[i+1];
+        result = LUTE3(0, value1, value2, value3);
+
+        intermediate[i] = result;
+    }
+
+    for (x = 1; x < IMG_WIDTH - 1; x++) {
+        for (y = 1; y < IMG_HEIGHT - 1; y++) {
+            value1 = intermediate[(y-1) * IMG_WIDTH + x];
+            value2 = intermediate[(y  ) * IMG_WIDTH + x];
+            value3 = intermediate[(y+1) * IMG_WIDTH + x];
+            result = LUTE3(0, value1, value2, value3);
+            /* print single result to uart */
+            r=0;
+            r+=wruint64(buf+r,result);
+            r+=wrstring(buf+r,",");
+            uart_println(buf);
+
+        }
+
+    }
+
+}
+
+void run_debug()
+{
+    int x, y, i, r, result;
+    char buf[128];
+
+    for (x = 0; x < 256; x = x + 32) {
+        for (y = 0; y < 256; y = y + 32) {
+            for (i = 0; i < 256; i = i + 32) {
+                result = LUTE3(0, x, y, i);
+
+                r = 0;
+                r += wrstring(buf + r, "");
+                r += wruint64(buf + r, x);
+                r += wrstring(buf + r, ",");
+                r += wruint64(buf + r, y);
+                r += wrstring(buf + r, ",");
+                r += wruint64(buf + r, i);
+                r += wrstring(buf + r, ",");
+                r += wruint64(buf + r, result);
+                uart_println(buf);
+            }
+        }
+    }
+
+}
+
+int main_()
+{
+    char buf[128];
+    int r;
+    uint64_t cycles = 0;
+    write_raw_image_header();
+    cycles = get_cycles();
+#ifdef LUT
+    gauss_lut();
+#else
+#ifdef DEBUG
+    run_debug();
+#else
+    gauss_native();
 #endif
+#endif
+    uart_println("};");
+
+    cycles = get_cycles() - cycles;
+    r = 0;
+    r += wrstring(buf + r, "Cycles used = ");
+    r += wruint64(buf + r, cycles);
+    uart_println(buf);
     uart_exit(0);
     return 0;
 }
